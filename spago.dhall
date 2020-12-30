b253ad83b048c0c2b28c8ac29e323b71fed5a28e
Mariano Heller <marianofheller@gmail.com>
2020-12-29 23:17:53 -0300

    fetch logger

--- spago.dhall
+++ spago.dhall
@@ -9,6 +9,7 @@ You can edit this file as you like.
   , "effect"
   , "milkis"
   , "node-process"
+  , "now"
   , "option"
   , "psci-support"
   ]
--- src/App/Request.purs
+++ src/App/Request.purs
@@ -1,48 +1,61 @@
-module App.Request where
+module App.Request (fetch) where
 
 import Prelude
-
 import App.Resources (Endpoint)
-import Data.Maybe (Maybe, fromMaybe)
+import Data.Foldable (intercalate)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Effect (Effect)
 import Effect.Aff (Aff)
 import Effect.Class (liftEffect)
-import Foreign.Object as Object
+import Effect.Class.Console (log)
 import Milkis (Response)
-import Milkis as Milkis
+import Milkis as M
 import Milkis.Impl.Node (nodeFetch)
 import Node.Process (lookupEnv)
 import Option as Option
-import Prim.Row (class Union)
+import Record as Record
+import Type.Row (class Union)
 
 getTokenFromEnv :: Effect (Maybe String)
 getTokenFromEnv = lookupEnv "usertoken"
 
-authHeaderKey :: String
-authHeaderKey = "X-Auth"
+_fetch :: forall options trash. Union options trash M.Options => M.URL -> { method :: M.Method | options } -> Aff Response
+_fetch url opts = logger $ M.fetch nodeFetch url opts
 
-_fetch ::
-  forall options trash.
-  Union
-    options
-    trash
-    Milkis.Options =>
-  Milkis.URL ->
-  { method :: Milkis.Method
-  | options
-  } ->
-  Aff Response
-_fetch = Milkis.fetch nodeFetch
+logger :: Aff Response -> Aff Response
+logger resAff = do
+  res <- resAff
+  liftEffect $ log $ formatResponse res
+  resAff
+
+formatResponse :: Response -> String
+formatResponse res =
+  let
+    url = show $ M.url res
+
+    status = show $ M.statusCode res
+
+    corch str = "[" <> str <> "]"
+  in
+    intercalate " "
+      $ [ (corch status)
+        , url
+        ]
 
 fetch :: Endpoint -> Aff Response
 fetch endpoint = do
+  token <- liftEffect getTokenFromEnv
   let
     record = Option.recordToRecord endpoint
 
-    body = fromMaybe "" record.body
-  token <- liftEffect getTokenFromEnv
-  _fetch record.url
-    { method: record.method
-    , body
-    , headers: Object.singleton authHeaderKey (fromMaybe "NO-TOKEN" token)
-    }
+    baseConfig =
+      { method: record.method
+      , headers: M.makeHeaders { "X-Auth": (fromMaybe "NO-TOKEN" token) }
+      }
+
+    goFetch = _fetch record.url
+
+    goFetchWithBody = _fetch record.url
+  case record.body of
+    Nothing -> goFetch baseConfig
+    Just body -> goFetchWithBody $ Record.merge baseConfig { body }
