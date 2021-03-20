43ba996ed557ef2721b87709604ab8342aad6997
Mariano Heller <marianofheller@gmail.com>
2021-03-20 10:58:11 -0300

    passing test

--- test/App.purs
+++ test/App.purs
@@ -1,9 +1,12 @@
 module Test.App (spec) where
 
 import Prelude
-import Test.Spec (SpecT, describe, it)
 import App (AppF(..))
 import App as App
+import App.Data.Clases (Clases)
+import App.Data.Creds (Creds)
+import App.Data.ReservaResult (ReservaResult)
+import App.Data.Reservas (Reservas)
 import App.Env as Env
 import App.Resources (Resource)
 import App.Resources as Resources
@@ -16,6 +19,7 @@ import Data.Either (Either(..))
 import Data.Enum (toEnum)
 import Data.Maybe (fromJust)
 import Data.Newtype (unwrap)
+import Data.String (joinWith)
 import Data.Traversable (for)
 import Dotenv (loadContents) as Dotenv
 import Effect.Aff (Aff)
@@ -24,14 +28,27 @@ import Effect.Exception (throw)
 import Node.Encoding (Encoding(..))
 import Node.FS.Aff (readTextFile)
 import Partial.Unsafe (unsafePartial)
+import Test.Spec (SpecT, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-mockFetch :: forall a. DecodeJson a => Resource a -> Aff a
-mockFetch _ = do
-  a <- Argonaut.decodeJson <$> Argonaut.fromString <$> (readTextFile UTF8 "data/login.json")
-  case a of
-    Left e -> liftEffect $ throw $ show e
+mockFetch_ :: forall a. DecodeJson a => String -> Resource a -> Aff a
+mockFetch_ path _ = do
+  fileStr <- readTextFile UTF8 path
+  case Argonaut.decodeJson =<< Argonaut.parseJson fileStr of
+    Left e -> liftEffect $ throw $ joinWith "\n" [ path, show e ]
     Right v -> pure v
+
+mockFetchLogin :: Resource Creds -> Aff Creds
+mockFetchLogin r = mockFetch_ "data/login.json" r
+
+mockFetchReservas :: Resource Reservas -> Aff Reservas
+mockFetchReservas r = mockFetch_ "data/reservas.json" r
+
+mockFetchClases :: Resource Clases -> Aff Clases
+mockFetchClases r = mockFetch_ "data/classes.json" r
+
+mockPostReserva :: Resource ReservaResult -> Aff ReservaResult
+mockPostReserva r = mockFetch_ "data/reservaSuccess.json" r
 
 mockDate :: Date
 mockDate =
@@ -52,16 +69,16 @@ appMockI = case _ of
   (GetLowerDateBound next) -> do
     pure $ next mockDate
   (Login url auth input next) -> do
-    creds <- mockFetch $ Resources.login url auth input
+    creds <- mockFetchLogin $ Resources.login url auth input
     pure $ next creds
   (GetReservas url creds next) -> do
-    reservas <- mockFetch $ Resources.misReservas url creds
+    reservas <- mockFetchReservas $ Resources.misReservas url creds
     pure $ next reservas
   (GetClases url creds next) -> do
-    clases <- mockFetch $ Resources.clases url creds
+    clases <- mockFetchClases $ Resources.clases url creds
     pure $ next clases
   (PostReservas url creds clases next) -> do
-    results <- for (unwrap clases) (mockFetch <<< Resources.reserva url creds)
+    results <- for (unwrap clases) (mockPostReserva <<< Resources.reserva url creds)
     pure $ next results
 
 spec :: forall i. Monad i => SpecT Aff Unit i Unit
